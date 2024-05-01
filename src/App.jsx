0692faf669f41662fc2a7480766339d39e29dbe9
Suraj Yadav <surajyadavy676@gmail.com>
2024-05-01 22:38:25 +0530

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <h1 className="text-center">Kanban App</h1>
+      <h1 className="text-center text-2xl mt-5">Kanban App</h1>
+      <div className="text-center mt-5">
+      <button className="bg-slate-500 py-1 px-8 mx-4 rounded-sm my-4">Login</button>
+      <button className="bg-yellow-100 py-1 px-8 mx-4 rounded-sm">Register</button>
+      </div>
     </>
   )
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <input type="text" placeholder='Enter Your Email' />
+      <input type="text" placeholder='Enter Your Password' />
+      <button>Submit</button>
+    </div>
+  )
+}
+
+export default Login
